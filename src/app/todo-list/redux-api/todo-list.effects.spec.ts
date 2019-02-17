f53c905461f7300b6cd1bb7f653cb8a48e7bd1a1
Christian Ludemann <ext.christian.lude@ef.com>
2019-02-17 22:04:02 +0100

    test: set up test cases for todo list effects

---
+++ src/app/todo-list/redux-api/todo-list.effects.spec.ts
@@ -0,0 +1,8 @@
+// https://github.com/blove/ngrx-testing/blob/master/src/app/state/user/user.effects.spec.ts
+describe('TodoListEffects', () => {
+  describe('loadTodoList', () => {
+    it('should return a stream with todo list loaded action', () => {});
+
+    it('should fail and return an action with the error', () => {});
+  });
+});
