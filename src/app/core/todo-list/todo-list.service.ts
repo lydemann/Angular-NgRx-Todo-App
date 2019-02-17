a6062faa60025cc78ff460aa1e324a6f96996f36
Christian Ludemann <ext.christian.lude@ef.com>
2019-02-17 21:42:19 +0100

    feat: todo list should be fetched from method

--- src/app/core/todo-list/todo-list.service.ts
+++ src/app/core/todo-list/todo-list.service.ts
@@ -2,34 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TODOItem } from '@app/shared/models/todo-item';
 import { of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, tap } from 'rxjs/operators';
 
 @Injectable()
 export class TodoListService {
+  private _todoList: TODOItem[] = [];
 
-    private _todoList: TODOItem[] = [];
+  public get todoList(): TODOItem[] {
+    return this._todoList;
+  }
 
-    public get todoList(): TODOItem[] {
-        return this._todoList;
-    }
+  private todoListUrl = '//localhost:8080/api/todo-list';
 
-    private todoListUrl = '//localhost:8080/api/todo-list';
+  constructor(private httpClient: HttpClient) {}
 
-    constructor(httpClient: HttpClient) {
-        httpClient.get<Array<TODOItem>>(this.todoListUrl).subscribe(data => {
-            this._todoList = data;
-        });
-    }
+  public getTodos() {
+    return this.httpClient.get<Array<TODOItem>>(this.todoListUrl).pipe(
+      tap((data) => {
+        this._todoList = data;
+      })
+    );
+  }
 
-    public addTodo(todo: TODOItem) {
-        return of(null).pipe(delay(2000));
-    }
+  public addTodo(todo: TODOItem) {
+    return of(null).pipe(delay(2000));
+  }
 
-    public updateTodo(todo: TODOItem) {
-        return of(null).pipe(delay(2000));
-    }
+  public updateTodo(todo: TODOItem) {
+    return of(null).pipe(delay(2000));
+  }
 
-    public deleteTodo(id: string) {
-        return of(null).pipe(delay(2000));
-    }
+  public deleteTodo(id: string) {
+    return of(null).pipe(delay(2000));
+  }
 }
