58c661c5715fe280250eab6d53235d5009c250b3
Christian Ludemann <ext.christian.lude@ef.com>
2019-02-17 21:41:50 +0100

    feat: setup store module

---
+++ src/app/core/store/store.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'environments/environment';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Testing Store DevTools',
+      logOnly: environment.production
+    })
+  ],
+  declarations: []
+})
+export class StateModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: StateModule
+    };
+  }
+
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule?: StateModule
+  ) {
+    if (parentModule) {
+      throw new Error('StateModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
