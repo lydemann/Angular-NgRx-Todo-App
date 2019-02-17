073cb227af1357230edb930a23b28d059d65a514
Christian Ludemann <ext.christian.lude@ef.com>
2019-02-17 22:04:20 +0100

    test: set up test cases for todo list reducers

---
+++ src/app/todo-list/redux-api/todo-list.reducers.spec.ts
@@ -0,0 +1,25 @@
+describe('TodoList reducer', () => {
+  describe('loadTodoItems', () => {
+    it('should set isLoading to true', () => {});
+  });
+
+  describe('todoItemsLoadFailed', () => {
+    it('should set isLoading to false and set error', () => {});
+  });
+
+  describe('todoItemCreatedReducer', () => {
+    it('should add new todo to todo list', () => {});
+  });
+
+  describe('todoItemDeletedReducer', () => {
+    it('should delete todo from todo list', () => {});
+  });
+
+  describe('todoItemUpdatedReducer', () => {
+    it('should update todo item', () => {});
+  });
+
+  describe('todoItemCompletedReducer', () => {
+    it('should complete given todo item', () => {});
+  });
+});
