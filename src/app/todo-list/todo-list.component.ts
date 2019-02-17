bde3d59c72d666f0d1e4b0e4f79b646bea74545e
Christian Ludemann <ext.christian.lude@ef.com>
2019-02-17 21:42:53 +0100

    feat: update todo-list to work with redux

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,26 +1,32 @@
-import { Component } from '@angular/core';
-import { TodoListService } from '@app/core/todo-list/todo-list.service';
+import { Component, OnInit } from '@angular/core';
 import { TODOItem } from '@app/shared/models/todo-item';
+import { TodoListActions } from './redux-api/todo-list.actions';
+import { TodoListSelector } from './redux-api/todo-list.selector';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  // TODO: current todo item i store
   public currentTODO: TODOItem = new TODOItem('', '');
+  public todoList$ = this.todoListSelector.getTodoList();
 
-  constructor(private todoListService: TodoListService) {}
+  constructor(
+    private todoListSelector: TodoListSelector,
+    private todoListActions: TodoListActions
+  ) {}
 
-  get todoList() {
-    return this.todoListService.todoList;
+  ngOnInit(): void {
+    this.todoListActions.loadTodoList();
   }
 
   public deleteTodo(id: string) {
-    const deleteIndex = this.todoListService.todoList.findIndex((todo) => todo.id === id);
-    this.todoListService.todoList.splice(deleteIndex, 1);
+    this.todoListActions.deleteTodo(id);
   }
 
+  // TODO: dispatch action
   public editTodo(todoItem: TODOItem) {
     this.currentTODO = todoItem;
   }
--- src/app/todo-list/todo-list.module.ts
+++ src/app/todo-list/todo-list.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@app/shared/shared.module';
 import { TodoListComponent } from '@app/todo-list/todo-list.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { AddTodoModule } from './add-todo/add-todo.module';
+import { TodoListEffects } from './redux-api/todo-list.effects';
+import { TodoListReducers } from './redux-api/todo-list.reducers';
+import { TodoListSelector } from './redux-api/todo-list.selector';
 
 @NgModule({
-  imports: [FormsModule, CommonModule, SharedModule, AddTodoModule],
-  declarations: [TodoListComponent]
+  imports: [
+    FormsModule,
+    CommonModule,
+    SharedModule,
+    AddTodoModule,
+    StoreModule.forFeature('todoList', TodoListReducers),
+    EffectsModule.forFeature([TodoListEffects])
+  ],
+  declarations: [TodoListComponent],
+  providers: [TodoListSelector]
 })
 export class TodoListModule {}
